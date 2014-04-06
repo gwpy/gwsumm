2d3c559029932cecf8feed51b6a3b087795a576e
Duncan Macleod <duncan.macleod@ligo.org>
2014-04-06 11:21:38 -0500

    utils.count_free_cores: don't use active_children

--- gwsumm/utils.py
+++ gwsumm/utils.py
@@ -143,4 +143,5 @@ def split_channels(channelstring):
 def count_free_cores(max=cpu_count()):
     """Count the number of CPU cores not currently used by this job.
     """
-    return max - (len(active_children()) + 1)
+    active = 1 #len(active_children()
+    return max - (active + 1)

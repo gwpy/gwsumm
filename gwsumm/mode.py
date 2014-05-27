9c135cc344c937b0b7b3372e0a3bcdf0ecb30470
Duncan Macleod <duncan.macleod@ligo.org>
2014-05-27 11:58:24 -0500

    mode: improved mode settings with new methods

--- gwsumm/mode.py
+++ gwsumm/mode.py
@@ -19,7 +19,9 @@
 """Job modes
 """
 
-from . import version
+import os.path
+
+from . import (globalv, version)
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
 __version__ = version.version
@@ -40,3 +42,47 @@ MODE_NAME = {SUMMARY_MODE_GPS: 'GPS',
 MODE_ENUM = dict((val, key) for (key, val) in MODE_NAME.iteritems())
 
 MODE = None
+
+
+def get_mode():
+    """Return the current mode.
+    """
+    return globalv.MODE
+
+
+def set_mode(m):
+    """Set the current mode.
+    """
+    if not isinstance(m, int):
+        m = MODE_ENUM[str(m).upper()]
+    globalv.MODE = m
+
+
+def get_base(date, mode=None):
+    """Determine the correct base attribute for the given date and mode.
+
+    Parameters
+    ----------
+    date : :class:`datetime.datetime`
+        formatted date
+    mode : `int`, `str`
+        enumerated interger code (or name) for the required mode
+
+    Returns
+    -------
+    base : `str`
+        the recommended base URL to have a correctly linked calendar
+    """
+    if mode is None:
+        mode = get_mode()
+    elif not isinstance(mode, int):
+        mode = MODE_ENUM[str(mode).upper()]
+    if mode == SUMMARY_MODE_DAY:
+        return os.path.join('day', date.strftime('%Y%m%d'))
+    elif mode == SUMMARY_MODE_WEEK:
+        return os.path.join('week', date.strftime('%Y%m%d'))
+    elif mode == SUMMARY_MODE_MONTH:
+        return os.path.join('month', date.strftime('%Y%m'))
+    elif mode == SUMMARY_MODE_YEAR:
+        return os.path.join('year', date.strftime('%Y'))
+    raise ValueError("Cannot format base for unknown mode %r" % mode)

09c378be5f1bb53cde0a260b67e759a53c084140
Duncan Macleod <duncan.macleod@ligo.org>
2014-09-23 14:02:01 -0500

    DataTab.process: improved mp plot error printing

--- gwsumm/tabs/data.py
+++ gwsumm/tabs/data.py
@@ -360,7 +360,7 @@ class DataTab(DataTabBase):
                         plot.process()
                         sleep(1)
                     except Exception as e:
-                        vprint('%s caught: %s' % (type(e).__name__, str(e)),
+                        vprint('%s caught: %s\n' % (type(e).__name__, str(e)),
                                stream=sys.stderr)
                     finally:
                         queue.task_done()

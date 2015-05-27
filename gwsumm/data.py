b2770fa3f2acd52898f9a99c7ae2c39626fbe66d
Duncan Macleod <duncan.macleod@ligo.org>
2015-05-27 10:27:27 -0500

    get_timeseries_dict: delete filter for multiprocessing
        - lambda filters don't pickle so need to delete them and reinstate
          them after i/o

--- gwsumm/data.py
+++ gwsumm/data.py
@@ -582,6 +582,10 @@ def _get_timeseries_dict(channels, segments, config=ConfigParser(),
                                       connection=ndsconnection, type=ndstype,
                                       **ioargs)
             else:
+                if nproc > 1:
+                    for c in qchannels:
+                        if c.ndsname in filter_:
+                            del c.filter
                 # pad resampling
                 if segment[1] == cachesegments[-1][1] and qresample:
                     resamplepad = 8
@@ -608,6 +612,9 @@ def _get_timeseries_dict(channels, segments, config=ConfigParser(),
                                      start=segstart, end=segend, type=ctype,
                                      nproc=nproc, resample=qresample,
                                      verbose=verbose, **ioargs)
+                for c in qchannels:
+                    if c.ndsname in filter_:
+                        c.filter = filter_[c.ndsname]
             for (channel, data) in tsd.iteritems():
                 if (channel.ndsname in globalv.DATA and
                     data.span in globalv.DATA[channel.ndsname].segments):

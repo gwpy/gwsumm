38b75e5c6cca4cbc63c91a8dd2fdc58f2e80dadf
Duncan Macleod <duncan.macleod@ligo.org>
2015-06-15 15:08:11 -0700

    Error404Tab: new tab for custom 404 output
        - writes custom 404 with some sensible redirect suggestions
        - bin/gw_summary now writes .htaccess as well
        - should .htaccess writing got into 404 tab processing?

--- gwsumm/tabs/builtin.py
+++ gwsumm/tabs/builtin.py
@@ -775,3 +775,50 @@ class AboutTab(SummaryArchiveMixin, Tab):
             html.about_this_page(config=config), **kwargs)
 
 register_tab(AboutTab)
+
+
+class Error404Tab(SummaryArchiveMixin, Tab):
+    type = '404'
+
+    def __init__(self, start, end, name='404', mode=None, **kwargs):
+        super(Error404Tab, self).__init__(name, **kwargs)
+        self.span = (start, end)
+        self.mode = mode
+
+    def write_html(self, config=list(), top=None, **kwargs):
+        if top is None:
+            top = kwargs.get('base', self.path)
+        kwargs.setdefault('title', '404: Page not found')
+        page = html.markup.page()
+        page.div(class_='alert alert-danger')
+        page.p()
+        page.strong("The page you are looking for doesn't exist")
+        page.p.close()
+        page.p("This could be because the times for which you are looking "
+               "were never processed (or haven't even happened yet), or "
+               "because no page exists for the specific data products you "
+               "want. Either way, if you think this is in error, please "
+               "contact <a class=\"alert-link\" "
+               "href=\"mailto:detchar+code@ligo.org\">the DetChar group</a>.")
+        page.p("Otherwise, you might be interested in one of the following:")
+        page.div(style="padding-top: 10px;")
+        page.a("Take me back", role="button", class_="btn btn-lg btn-info",
+               title="Back", href="javascript:history.back()")
+        page.a("Take me up one level", role="button",
+               class_="btn btn-lg btn-warning", title="Up",
+               href="javascript:linkUp()")
+        page.a("Take me to the top level", role="button",
+               class_="btn btn-lg btn-success", title="Top", href=top)
+        page.div.close()
+        page.div.close()
+        page.script("""
+  function linkUp() {
+    var url = window.location.href;
+    if (url.substr(-1) == '/') url = url.substr(0, url.length - 2);
+    url = url.split('/');
+    url.pop();
+    window.location = url.join('/');
+  }""", type="text/javascript")
+        return super(Error404Tab, self).write_html(page, **kwargs)
+
+register_tab(Error404Tab)

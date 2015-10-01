2e7eea7f65a08a34a92d847b869b9cc4ee776a32
Duncan Macleod <duncan.macleod@ligo.org>
2015-10-01 10:37:30 -0700

    data: fixed some bugs in frametype parsing

--- gwsumm/data.py
+++ gwsumm/data.py
@@ -255,7 +255,7 @@ def get_timeseries_dict(channels, segments, config=ConfigParser(),
     # separate channels by type
     if query:
         if frametype is not None:
-            frametypes = {frametype: channels}
+            frametypes = {(None, frametype): channels}
         else:
             frametypes = dict()
             allchannels = set([
@@ -646,7 +646,7 @@ def get_spectrogram(channel, segments, config=ConfigParser(), cache=None,
     for c in channels:
         specs.append(_get_spectrogram(c, segments, config=config, cache=cache,
                                       query=query, nds=nds, format=format,
-                                      return_=return_,
+                                      return_=return_, frametype=frametype,
                                       multiprocess=multiprocess,
                                       datafind_error=datafind_error,
                                       **fftparams))

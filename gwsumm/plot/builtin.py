7890701ba171740e0a0b3e15366a684df32ec0b2
Duncan Macleod <duncan.macleod@ligo.org>
2015-11-04 14:41:07 -0600

    TimeSeriesDataPlot.add_state_segments: fixed epoch bug
    - manually reset epoch for parent axes

--- gwsumm/plot/builtin.py
+++ gwsumm/plot/builtin.py
@@ -73,12 +73,14 @@ class TimeSeriesDataPlot(DataLabelSvgMixin, DataPlot):
             :meth:`~gwpy.plotter.timeseries.TimeSeriesPlot.add_state_segments`
             method.
         """
+        epoch = ax.get_epoch()
         kwargs.setdefault('edgecolor', 'darkgreen')
         kwargs.setdefault('facecolor', GREEN)
         kwargs.setdefault('known', {'facecolor': 'red', 'edgecolor': 'darkred'})
         sax = self.plot.add_state_segments(self.state, ax, plotargs=kwargs)
+        ax.set_epoch(epoch)
+        sax.set_epoch(epoch)
         sax.tick_params(axis='y', which='major', labelsize=12)
-        sax.set_epoch(float(self.start))
         sax.yaxis.set_ticks_position('none')
         return sax
 

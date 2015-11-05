46ba4b825513a3cb56fa5aead58dfe122b67e9b5
Duncan Macleod <duncan.macleod@ligo.org>
2015-11-05 14:41:50 -0600

    plot: use mpl default in v1.5

--- gwsumm/plot/__init__.py
+++ gwsumm/plot/__init__.py
@@ -40,6 +40,8 @@ The available classes are:
    TriggerRateDataPlot
 """
 
+from matplotlib import __version__ as mpl_version
+
 from gwpy.plotter import rcParams
 rcParams.update({
     'figure.subplot.bottom': 0.17,
@@ -50,6 +52,10 @@ rcParams.update({
     'grid.color': 'gray',
     'svg.fonttype': 'none',
 })
+
+# use new viridis colormap by default
+if mpl_version >= '1.5':
+    rcParams['image.cmap'] = 'viridis'
 
 from gwsumm import version
 

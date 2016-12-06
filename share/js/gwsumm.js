b4cc6cb11ec8518e1a127c05dbc908c97aa74bfe
Thomas Massinger <thomas.massinger@ligo.org>
2016-12-05 16:18:11 -0800

    Moved a close-bracket

--- share/js/gwsumm.js
+++ share/js/gwsumm.js
@@ -209,8 +209,7 @@ $(window).load(function() {
                            scrolling: 'no',
                            beforeShow: function() {resizeFancyboxIframe()},
                            helpers: {overlay: {locked: false},
-                                     title: {type: 'inside'}
-                                    }
+                                     title: {type: 'inside'}}
   });
 
   // custom fancybox for stamp-pem bokeh plot

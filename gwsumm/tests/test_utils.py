011266408735edff38128c6e4aa6733c011c9c40
Duncan Macleod <duncan.macleod@ligo.org>
2017-08-16 12:37:52 +0100

    tests: rewrote test_utils.py in pytest style
    and extended the tests to increase the coverage

--- gwsumm/tests/test_utils.py
+++ gwsumm/tests/test_utils.py
@@ -26,116 +26,137 @@ import re
 import sys
 import shutil
 from math import pi
+from multiprocessing import cpu_count
 
-from common import unittest
+from six import string_types
+
+import pytest
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
 from gwsumm import (utils, globalv)
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
 
 
-class UtilsTestCase(unittest.TestCase):
-    def test_elapsed_time(self):
-        e = time.time() - globalv.START
-        self.assertTrue(utils.elapsed_time() - e < 1)
-
-    def test_get_odc_bitmask(self):
-        # test fast ODC
-        self.assertEqual(
-            utils.get_odc_bitmask('L1:TEST-ODC_CHANNEL_OUT_DQ'),
-            'L1:TEST-ODC_CHANNEL_BITMASK')
-        # test slow ODC
-        self.assertEqual(
-            utils.get_odc_bitmask('L1:TEST-ODC_CHANNEL_OUTMON'),
-            'L1:TEST-ODC_CHANNEL_BITMASK')
-        # test LATCH
-        self.assertEqual(
-            utils.get_odc_bitmask('L1:TEST-ODC_CHANNEL_LATCH'),
-            'L1:TEST-ODC_CHANNEL_BITMASK')
-        # test other (noop)
-        self.assertEqual(
-            utils.get_odc_bitmask('L1:TEST-CHANNEL_NAME'),
-            'L1:TEST-CHANNEL_NAME')
-
-    def test_get_default_ifo(self):
-        self.assertEqual(
-            utils.get_default_ifo('host.ligo-la.caltech.edu'), 'L1')
-        self.assertEqual(
-            utils.get_default_ifo('host.ligo-wa.caltech.edu'), 'H1')
-        self.assertEqual(
-            utils.get_default_ifo('host.atlas.aei.uni-hannover.de'), 'G1')
-        self.assertEqual(
-            utils.get_default_ifo('host.virgo.ego.it'), 'V1')
-        self.assertRaises(ValueError, utils.get_default_ifo,
-                          'host.ligo.caltech.edu')
-
-    def test_safe_eval(self):
-        # test normal string
-        self.assertEqual(utils.safe_eval('test'), 'test')
-        self.assertEqual(utils.safe_eval('my random content'),
-                         'my random content')
-        # test float/int
-        t = utils.safe_eval('1')
-        self.assertEqual(t, 1)
-        self.assertIsInstance(t, int)
-        t = utils.safe_eval('1.')
-        self.assertEqual(t, 1.)
-        self.assertIsInstance(t, float)
-        # test tuple/list
-        t = utils.safe_eval('1,')
-        self.assertEqual(t, (1,))
-        self.assertIsInstance(t, tuple)
-        t = utils.safe_eval('1,2,\'test\',4')
-        self.assertEqual(t, (1, 2, 'test', 4))
-        # test with math
-        t = utils.safe_eval("[], [0], 1/(2*pi)")
-        self.assertIsInstance(t, tuple)
-        self.assertEqual(t, ([], [0], 1/(2*pi)))
-        # test lambda
-        t = utils.safe_eval("lambda x: x**2")
-        self.assertTrue(callable(t))
-        self.assertEqual(t(4), 16)
-        # test unsafe
-        self.assertRaises(ValueError, utils.safe_eval,
-                          "os.remove('file-that-doesnt-exist')")
-        self.assertRaises(ValueError, utils.safe_eval,
-                          "lambda x: shutil.remove('file-that-doesnt-exist')")
-        # test locals or globals
-        t = utils.safe_eval('test', globals_={'test': 4})
-        self.assertEqual(t, 4)
-        t = utils.safe_eval('type(self)', locals_={'self': self})
-        self.assertEqual(t, type(self))
-
-    def test_mkdir(self):
-        d = 'test-dir/test-dir2'
-        try:
-            utils.mkdir(d)
-        finally:
-            if os.path.isdir(d):
-                shutil.rmtree(d)
-
-    def test_nat_sorted(self):
-        # sorted strings numerically
-        self.assertListEqual(
-            utils.nat_sorted(['1', '10', '2']), ['1', '2', '10'])
+def test_elapsed_time():
+    e = time.time() - globalv.START
+    assert utils.elapsed_time() - e < .1
 
 
-class TestVprint(object):
-    # this is separate from the other tests so we can use pytest's
-    # capsys fixture to check stdout for content when testing vprint()
+def test_vprint(capsys):
+    # test non-verbose
+    globalv.VERBOSE = False
+    utils.vprint('anything', stream=sys.stdout)
+    out, err = capsys.readouterr()
+    assert out == ''
 
-    def test_vprint(self, capsys):
-        # test non-verbose
-        globalv.VERBOSE = False
-        utils.vprint('anything', stream=sys.stdout)
-        out, err = capsys.readouterr()
-        assert out == ''
-        # test verbose
-        globalv.VERBOSE = True
-        utils.vprint('anything', stream=sys.stdout)
-        out, err = capsys.readouterr()
-        assert out == 'anything'
-        # test profiled
-        globalv.PROFILE = True
-        utils.vprint('anything\n', stream=sys.stdout)
-        out, err = capsys.readouterr()
-        assert re.match('\Aanything \(\d+\.\d\d\)\n\Z', out) is not None
+    # test verbose
+    globalv.VERBOSE = True
+    utils.vprint('anything', stream=sys.stdout)
+    out, err = capsys.readouterr()
+    assert out == 'anything'
+
+    # test profiled
+    globalv.PROFILE = True
+    utils.vprint('anything\n', stream=sys.stdout)
+    out, err = capsys.readouterr()
+    assert re.match('\Aanything \(\d+\.\d\d\)\n\Z', out) is not None
+
+
+def test_mkdir():
+    d = 'test-dir/test-dir2'
+    try:
+        utils.mkdir(d)
+        assert os.path.isdir(d)
+    finally:
+        if os.path.isdir(d):
+            shutil.rmtree(d)
+
+
+def test_nat_sorted():
+    # sorted strings numerically
+    assert utils.nat_sorted(['1', '10', '2', 'a', 'B']) == [
+        '1', '2', '10', 'B', 'a']
+
+
+def test_which():
+    environ = os.environ.copy()
+    os.environ['PATH'] = '/usr/bin:/bin'
+    try:
+        with mock.patch('os.path.isfile', return_value=True), \
+                mock.patch('os.access', return_value=True):
+            assert utils.which('/bin/bash') == '/bin/bash'
+            assert utils.which('bash') == '/usr/bin/bash'
+        with mock.patch('os.path.isfile', return_value=False):
+            assert utils.which('blah') is None
+    finally:
+        os.environ = environ
+
+
+def test_count_free_cores():
+    assert utils.count_free_cores() == cpu_count() - 2
+    assert utils.count_free_cores(2) == 0
+
+
+@pytest.mark.parametrize('chan, mask', [
+    ('L1:TEST-ODC_CHANNEL_OUT_DQ', 'L1:TEST-ODC_CHANNEL_BITMASK'),
+    ('L1:TEST-ODC_CHANNEL_OUTMON', 'L1:TEST-ODC_CHANNEL_BITMASK'),
+    ('L1:TEST-ODC_CHANNEL_LATCH', 'L1:TEST-ODC_CHANNEL_BITMASK'),
+    ('L1:TEST-CHANNEL', 'L1:TEST-CHANNEL')
+])
+def test_get_odc_bitmask(chan, mask):
+    assert utils.get_odc_bitmask(chan) == mask
+
+
+@pytest.mark.parametrize('value, out', [
+    ('my random content', 'my random content'),
+    ('1', 1),
+    ('1.', 1.),
+    ('1,', (1,)),
+    ('1,2,\'test\',4', (1, 2, 'test', 4)),
+    ('[], [0], 1/(2*pi)', ([], [0], 1/(2*pi))),
+    ('lambda x: x**2', lambda x: x ** 2),
+    (pytest, pytest),
+])
+def test_safe_eval(value, out):
+    evalue = utils.safe_eval(value)
+    assert type(evalue) is type(out)
+
+    if not isinstance(value, string_types):
+        assert evalue is out
+    elif callable(out):
+        assert evalue(4) == out(4)
+    else:
+        assert evalue == out
+
+
+def test_safe_eval_2():
+    # test unsafe
+    with pytest.raises(ValueError) as exc:
+        utils.safe_eval("os.remove('file-that-doesnt-exist')")
+    assert str(exc.value).startswith('Will not evaluate string containing')
+
+    with pytest.raises(ValueError):
+        utils.safe_eval("lambda x: shutil.remove('file-that-doesnt-exist')")
+
+    # test locals or globals
+    assert utils.safe_eval('test', globals_={'test': 4}) == 4
+    assert utils.safe_eval('type(self)',
+                           locals_={'self': globalv}) == type(globalv)
+
+
+@pytest.mark.parametrize('ifo, host', [
+    ('G1', 'host.atlas.aei.uni-hannover.de'),
+    ('H1', 'host.ligo-wa.caltech.edu'),
+    ('L1', 'host.ligo-la.caltech.edu'),
+    ('V1', 'host.virgo.ego.it'),
+])
+def test_get_default_ifo(ifo, host):
+    assert utils.get_default_ifo(host) == ifo
+
+    with pytest.raises(ValueError):
+        utils.get_default_ifo('host.ligo.caltech.edu')

d26863484188e42e11b52ac24de5cd7e08d5ebb9
Derek Davis <derek.davis@ldas-pcdev1.ligo-wa.caltech.edu>
2023-05-03 12:29:43 -0700

    Add extra line for lint

--- gwsumm/plot/range.py
+++ gwsumm/plot/range.py
@@ -55,6 +55,7 @@ RAN_DICT = {
         'burst_range_psd': gwpy.astro.burst_range_spectrum,
 }
 
+
 def _get_params(keys, pargs, nchans=1):
     """Return a `dict` of `list` of plot arguments for every channel
     """
@@ -113,6 +114,7 @@ class RangePlotMixin(object):
             # replace range_func arg with correct method
             if 'range_func' in rangekwargs.keys():
                 rangekwargs['range_func'] = RAN_DICT[rangekwargs['range_func']]
+                print(rangekwargs['range_func'])
             rlist = self.range_func(channel, valid, query=self.read,
                                     **fftkwargs, **rangekwargs)
             try:
